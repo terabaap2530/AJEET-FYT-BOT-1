70998722f2f53611f37fec9b8caec778b43bd896
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-09 00:11:46 +0530

    Update and rename 0antibd.js to 0_ANTI-NAME.js

--- modules/events/0_ANTI-NAME.js
+++ modules/events/0_ANTI-NAME.js
@@ -1,9 +1,9 @@
 module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
-  version: "0.0.1",//beta
-  credits: "Mr Faris",
-  description: "Against changing Bot's nickname"
+  version: "0.0.1",
+  credits: "PREM BABU",
+  description: "THIS BOT WAS MADE BY MR PREM BABU"
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} BOSS AAP MERA NAME CHANGE NAHI KAR SAKTE 🙂✌️`}, threadID);
+       return api.sendMessage({ body: `😆😆😆😆😆`}, threadID);
     }  
         }
