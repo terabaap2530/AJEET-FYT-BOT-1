0fd8dc55e5047e2c6e83f8e2a5d7d897041b2a06
terabaap2530 <terabaap2530@gmail.com>
2025-09-04 16:08:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
